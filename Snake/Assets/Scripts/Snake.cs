37221ac0bcf423627bece4ae5343f2230aa121f7
Yakir <yakirh95@gmail.com>
2021-03-21 21:27:36 +0200

    Snake movement now works through update

--- Snake/Assets/Scripts/Snake.cs
+++ Snake/Assets/Scripts/Snake.cs
@@ -4,15 +4,14 @@ using UnityEngine;
 
 public class Snake : MonoBehaviour
 {
-    Vector2Int gridMoveDirection;
-    Vector2Int gridPosition;
+    Vector2 gridMoveDirection;
+    Vector2 gridPosition;
+    const float speed = 0.009f;
 
     private void Start()
     {
         gridPosition = new Vector2Int(0, 0);
         gridMoveDirection = new Vector2Int(0, 0);
-
-        InvokeRepeating("HandleMovementPeriods", 0, 0.7f);
     }
 
     private void Update()
@@ -22,11 +21,11 @@ public class Snake : MonoBehaviour
 
     void HandleInput()
     {
-        if (Input.GetKeyDown(KeyCode.UpArrow) && (gridMoveDirection.y != -1))
+        if (Input.GetKeyDown(KeyCode.UpArrow) && (gridMoveDirection.y >= 0))
         {
             transform.rotation = Quaternion.Euler(0,0,0);
             gridMoveDirection.x = 0;
-            gridMoveDirection.y = 1;
+            gridMoveDirection.y = speed;
         }
 
         if (Input.GetKeyDown(KeyCode.DownArrow) && (gridMoveDirection.y != 1))
@@ -34,14 +33,14 @@ public class Snake : MonoBehaviour
             transform.rotation = Quaternion.Euler(0, 0, 180);
 
             gridMoveDirection.x = 0;
-            gridMoveDirection.y = -1;
+            gridMoveDirection.y = -speed;
         }
 
         if (Input.GetKeyDown(KeyCode.RightArrow) && (gridMoveDirection.x != -1))
         {
             transform.rotation = Quaternion.Euler(0, 0, 270);
 
-            gridMoveDirection.x = 1;
+            gridMoveDirection.x = speed;
             gridMoveDirection.y = 0;
         }
 
@@ -49,13 +48,10 @@ public class Snake : MonoBehaviour
         {
             transform.rotation = Quaternion.Euler(0, 0, 90);
 
-            gridMoveDirection.x = -1;
+            gridMoveDirection.x = -speed;
             gridMoveDirection.y = 0;
         }
-    }
 
-    void HandleMovementPeriods()
-    {
         gridPosition += gridMoveDirection;
         transform.position = new Vector2(gridPosition.x, gridPosition.y);
     }
