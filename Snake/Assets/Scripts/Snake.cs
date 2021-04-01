c64961095ecb6fae287611dba714a938d15b0480
Yakir <yakirh95@gmail.com>
2021-04-01 14:34:06 +0300

    updated

--- Snake/Assets/Scripts/Snake.cs
+++ Snake/Assets/Scripts/Snake.cs
@@ -6,10 +6,10 @@ using System.Linq;
 public class Snake : MonoBehaviour
 {
     Vector2 moveDirection = Vector2.up;
-    public List<Transform> tail = new List<Transform>();
+    List<Transform> tail = new List<Transform>();
 
     bool snakeAte = false;
-    public GameObject tailPart;
+    GameObject tailPart;
 
     bool turnRight;
     bool turnLeft;
